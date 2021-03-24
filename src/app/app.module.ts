285ce607de4213736bd4dc25d683145a35079203
Tanveer Ahmed <tanveerahmed0411@gmail.com>
2021-03-24 11:32:38 +0530

    03-AppBarComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppbarComponent } from './appbar/appbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
